3ba92d5e2acfbc4e955fcf1c64f49165e5c65873
Kyle138 <kyle138@gmail.com>
2022-03-29 22:38:24 -0500

    acidd() for bgc

--- js/acidd.js
+++ js/acidd.js
@@ -31,6 +31,7 @@ function getElems(prefix) {
 * If the backgroundColor isn't set, check the parent elements all the way up.
 * If no backgroundColor is set all the way up to HTML element, set to 255,255,255.
 * @cls {string} class to get background color for. eg: bgc-0 bgc-1 bgc-2 etc
+* Sets elements['bgr'][cls] with array of R,G,B values and sets the delta direction value to 1 initially.
 */
 function getbgcRGB(cls) {
   return new Promise((res) => {
@@ -50,7 +51,7 @@ function getbgcRGB(cls) {
       } while (rgb == 'rgba(0, 0, 0, 0)')
     }
     // The selected element has a color set, convert RGB values to array of numbers and return it.
-    elements['bgc'][cls] = rgb.split('(')[1].split(')')[0].split(', ').map(Number);
+    elements['bgc'][cls] = rgb.split('(')[1].split(')')[0].split(', ').map(x => [parseInt(x),1]);
     return res();
   }); // End Promise
 } // End getbgcRGB
@@ -58,13 +59,49 @@ function getbgcRGB(cls) {
 /*
 * Gets the color for the first element of the selected class.
 * @cls {string} class to get background color for. eg: bgc-0 bgc-1 bgc-2 etc
+* Sets elements['clr'][cls] with array of R,G,B values and sets the delta direction value to 1 initially.
 */
 function getclrRGB(cls) {
   return new Promise((res) => {
-    elements['clr'][cls] = window.getComputedStyle(document.querySelector(`.${cls}`),null).color.split('(')[1].split(')')[0].split(', ').map(Number);
+    elements['clr'][cls] = window.getComputedStyle(document.querySelector(`.${cls}`),null).color.split('(')[1].split(')')[0].split(', ').map(x => [parseInt(x),1]);
     return res();
   });
 } // End getclrRGB
+
+/*
+* Reckons a random delta to all clr and bgc RGB values in the element array.
+*/
+function acidd() {
+  console.log('acidd!');  // DEBUG:
+  Object.keys(elements['bgc']).forEach((elem) => {
+    console.log(`elem: ${elem}`); // DEBUG:
+
+    // Randomize and add a delta to all 3 RGB values.
+    for(let i = 0; i<3;i++) {
+      elements['bgc'][elem][i][0] += (elements['bgc'][elem][i][1]*Math.floor(Math.random()*3));
+      // Check if we're below the lower boundary
+      if(elements['bgc'][elem][i][0] < 0) {
+        elements['bgc'][elem][i][1] *= -1;  // Change the directional
+        elements['bgc'][elem][i][0] = 6;  // Move away from the boundary
+      }
+      // Check if we're above the upper boundary
+      if(elements['bgc'][elem][i][0] > 255) {
+        elements['bgc'][elem][i][1] *= -1;  // Change the directional
+        elements['bgc'][elem][i][0] = 249; // Move away from the boundary
+      }
+    }
+
+    // Assign the new values to elements with the selected class
+    document.querySelectorAll(`.${elem}`).forEach((ment) => {
+      console.log(elements['bgc']);
+      ment.style.backgroundColor = `rgb(${elements['bgc'][elem][0][0]},${elements['bgc'][elem][1][0]},${elements['bgc'][elem][2][0]})`;
+      console.log(`background: ${ment.style.backgroundColor}`); // DEBUG:
+    })
+  }); // End bgc.forEach
+
+  window.setTimeout(acidd,333);	//Play it again Sam
+} // End acidd
+
 
 //**************
 // Main Function
@@ -81,7 +118,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Only continue if we have any bgc-* or clr-* classes
     if(Object.keys(elements['bgc']).length+Object.keys(elements['clr']).length > 0) {
-      console.log('acidd!');  // DEBUG:
 
       // Retrieve initial BGC and CLR values
       Promise.all(  // I make no promises that this is correct.
@@ -99,8 +135,8 @@ document.addEventListener("DOMContentLoaded", () => {
       .then(() => {
         console.log('then');  // DEBUG:
         console.log(elements); // DEBUG:
-      })  // End Promise.all.then
-      // *************  Call a promise function here ***************
+        // acidd();   //********************ARM/DISARM
+      });  // End Promise.all.then
 
     } // End if bgc + clr
   } // End if .acidd
