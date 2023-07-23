7be1185ae7c8ae4c617742bbc3889a2dbca96e92
kyle138 <kyle138@gmail.com>
2023-07-22 23:44:15 -0500

    Scroll to contact

--- js/contact.js
+++ js/contact.js
@@ -4,14 +4,21 @@ document.addEventListener("DOMContentLoaded", () => {
   const contactBtn = document.getElementById("contactme-btn");
   const contactFrm = document.getElementById("contact");
 
+  // delay
+  const delay = ms => new Promise(res => setTimeout(res, ms)); 
+
   // Intercept button clicks
-  contactBtn.addEventListener("click", () => {
+  contactBtn.addEventListener("click", async () => {
+
+    // Wait for form to expand
+    await delay(1138);
+
     // Scroll to contact form
     window.scrollTo({
       left: 0,
       top: contactFrm.offsetTop,
       behavior: "smooth" 
     });
-  });
 
-});
+  }); // End contactBtn.addEventListener
+}); // End DOMContentLoaded
